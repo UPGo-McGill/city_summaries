6d68397c5b923231955de1e3d372d58e56016c2b
d-girard <danielle.girard@mail.mcgill.ca>
2019-06-03 09:47:46 -0400

    start mapping script

---
+++ R/mapping.R
@@ -0,0 +1,28 @@
+##################### MAPPING ##########################################
+
+source("R/01_helper_functions.R")
+
+
+
+
+##
+figure1 <- 
+  tm_shape(st_buffer(city_border, 200)) +
+  tm_borders(lwd = 1) + 
+  tm_shape(streets)+
+  tm_lines(col="grey", alpha = 0.5)+
+  tm_shape(property)+
+  tm_dots(col = "Listing_Type",
+          scale = 4/3, 
+          palette = get_brewer_pal("-Dark2", n = 3), 
+          alpha = 0.6, 
+          legend.show = FALSE, 
+          size = "revenue", 
+          title.size = "Revenu", 
+          size.lim = c(0, 100000)) +
+  tm_add_legend(type="symbol",
+                col= get_brewer_pal("-Dark2", n = 3),
+                labels=c("Entire Home", "Private Room", "Shared Room"),
+                border.lwd = NA,
+                alpha = 0.6,
+                title="Listing Type")
