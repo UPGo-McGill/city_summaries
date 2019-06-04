43b47e3e94086e1cf291b53d57a66d5308782bfb
amy <amy.bolt@mail.mcgill.ca>
2019-06-04 15:58:56 -0400

    host revenue

--- R/04_graphing.R
+++ R/04_graphing.R
@@ -2,6 +2,12 @@
 
 source("R/01_helper_functions.R")
 
+# Set up timeframes
+year_prior <- as.POSIXlt(End_date)
+year_prior$year <- year_prior$year - 1
+year_prior_prior <- as.POSIXlt(year_prior)
+year_prior_prior$year <- year_prior$year - 1
+
 # Active airbnb listings over time
 
 figure2 <- ggplot(daily %>% 
@@ -10,4 +16,24 @@ figure2 <- ggplot(daily %>%
   geom_line(aes(Date, Listings)) 
 
 #ggsave("output/figure2.jpg")
+
+# host revenue
+host_revenue <-
+  daily %>%
+  filter(Date >= year_prior, Date <= End_date, Status == "R") %>%
+  group_by(Airbnb_HID) %>%
+  summarize(rev = sum(Price)) %>%
+  filter(rev > 0) %>%
+  summarize(
+    `Top 1%`  = sum(rev[rev > quantile(rev, c(0.99))] / sum(rev)),
+    `Top 5%`  = sum(rev[rev > quantile(rev, c(0.95))] / sum(rev)),
+    `Top 10%` = sum(rev[rev > quantile(rev, c(0.90))] / sum(rev))) %>% 
+  gather(`Top 1%`, `Top 5%`, `Top 10%`, key = "percentile", value = "value") %>% 
+  mutate(percentile = factor(percentile, levels = c('Top 1%', 'Top 5%', 'Top 10%')))
+
+ggplot(host_revenue)+
+  geom_bar(mapping = aes(x = percentile, y = value, fill = percentile), stat = "identity")
+
+#ggsave("output/figure3.jpg")
+
 
